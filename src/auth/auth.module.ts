8c5670b4094e8d1dc53160d74e7349ac0ec8404a
gustavokurtz <kurtzff32@gmail.com>
2025-04-09 18:08:40 -0300

    add module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,3 +1,4 @@
+// src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
@@ -5,14 +6,20 @@ import { AuthService } from './auth.service';
 import { AuthController } from '../auth/auth.controller';
 import { UserModule } from '../user/user.module';
 import { JwtStrategy } from '../auth/jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UserModule,
     PassportModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET_KEY, // Em produção, use variáveis de ambiente
-      signOptions: { expiresIn: '1h' },
+    ConfigModule, // Adicione esta linha
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET_KEY'),
+        signOptions: { expiresIn: '1h' },
+      }),
     }),
   ],
   providers: [AuthService, JwtStrategy],
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,12 @@
+// src/user/user.module.ts
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule], // Adicione esta linha
   controllers: [UserController],
   providers: [UserService, PrismaService],
   exports: [UserService],
